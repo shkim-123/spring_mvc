885d840327d7b8f30e57b7de2490aa17a34543a1
SH Kim <82145134+shkim-123@users.noreply.github.com>
2023-05-01 21:42:54 +0900

    spring mvc controller 실용

--- src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -4,8 +4,8 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
@@ -14,12 +14,12 @@ public class SpringMemberControllerV3 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
-    @RequestMapping("/new-form")
+    @GetMapping(value = "/new-form")
     public String newForm() {
         return "new-form";
     }
 
-    @RequestMapping("/save")
+    @PostMapping(value = "/save")
     public String save(
             @RequestParam("username") String username,
             @RequestParam("age") int age,
@@ -32,7 +32,7 @@ public class SpringMemberControllerV3 {
         return "save-result";
     }
 
-    @RequestMapping
+    @GetMapping
     public String members(Model model) {
 
         List<Member> members = memberRepository.findAll();
